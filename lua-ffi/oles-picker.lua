c6f4ec8766bfd74476b9ef8466d679f2ee8f4989
j0le <j0le@users.noreply.github.com>
2022-03-13 02:08:49 +0100

    Follow tutorial to write telescope picker

---
+++ lua-ffi/oles-picker.lua
@@ -0,0 +1,23 @@
+-- Tutorial https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md
+
+
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+
+
+-- our picker function: colors
+local colors = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "colors",
+    finder = finders.new_table {
+      results = { "red", "green", "blue" }
+    },
+    sorter = conf.generic_sorter(ots),
+  }):find()
+end
+
+-- to execute the function
+colors()
+
