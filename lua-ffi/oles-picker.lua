7a772ab53ebcb876ac503e6f6fcc9989d8c472bb
j0le <j0le@users.noreply.github.com>
2022-03-13 21:09:58 +0100

    Use entry maker
    pasting does not work anymore

--- lua-ffi/oles-picker.lua
+++ lua-ffi/oles-picker.lua
@@ -1,6 +1,7 @@
 -- Tutorial https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md
 
 
+
 local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
@@ -13,14 +14,25 @@ local colors = function(opts)
   pickers.new(opts, {
     prompt_title = "colors",
     finder = finders.new_table {
-      results = { "red", "green", "blue" }
+      results = {
+        { "red",   "#ff0000" },
+        { "green", "#00ff00" },
+        { "blue",  "#0000ff" },
+      },
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = entry[1],
+          ordinal = entry[1],
+        }
+      end
     },
     sorter = conf.generic_sorter(ots),
     attach_mappings = function(prompt_bufnr, map)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        -- print(vim.inspect(selection))
+        print(vim.inspect(selection))
         vim.api.nvim_put({ selection[1] }, "", false, true)
       end)
       return true
@@ -31,3 +43,5 @@ end
 -- to execute the function
 colors(require("telescope.themes").get_dropdown{})
 
+
+-- vim:tabstop=2:expandtab:
